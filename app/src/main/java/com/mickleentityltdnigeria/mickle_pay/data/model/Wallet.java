98922a7a444257dc838ebc30158e8dee4e39b22c
John Ifegwu <johnifegwu@outlook.com>
2021-04-17 06:51:44 +0100

    MICKLE-PAY

---
+++ app/src/main/java/com/mickleentityltdnigeria/mickle_pay/data/model/Wallet.java
@@ -0,0 +1,10 @@
+package com.mickleentityltdnigeria.mickle_pay.data.model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.io.Serializable;
+
+@IgnoreExtraProperties
+public class Wallet implements Serializable {
+
+}
